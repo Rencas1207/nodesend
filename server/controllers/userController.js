f251be8b79ee4e7b191ff18d2d7ee5f578607a79
Rencas <rencasdag.12@gmail.com>
2023-08-26 23:52:39 -0500

    hash passwords

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,9 @@
 import User from "../models/Users.js";
+import bcrypt from 'bcrypt';
 
 const newUser = async (req, res) => {
    // check if the user is already registered
-   const { email } = req.body;
+   const { email, password } = req.body;
 
    let user = await User.findOne({ email });
 
@@ -10,10 +11,21 @@ const newUser = async (req, res) => {
       return res.status(400).json({ msg: 'El usuario ya está registrado' })
    }
 
-   user = await new User(req.body);
-   user.save();
+   user = new User(req.body);
 
-   res.json({ msg: 'Usuario creado correctamente' })
+   //hass to password
+   const salt = await bcrypt.genSalt(10);
+   user.password = await bcrypt.hash(password, salt)
+
+
+   try {
+      await user.save();
+
+      res.json({ msg: 'Usuario creado correctamente' })
+   } catch (error) {
+      console.log(error);
+   }
+
 }
 
 export {
