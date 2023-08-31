ca266b18a6db04414e4474e40639c6cedc8260d8
Rencas <rencasdag.12@gmail.com>
2023-08-30 23:27:09 -0500

    routing for login & create account + styles with tailwind

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,28 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const Header = () => {
    return (
       <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-         <Image
-            width={64}
-            height={50}
-            className="w-64 mb-8 md:mb-0"
-            src="logo.svg"
-            alt="logo react node send"
-            priority
-         />
+         <Link href="/">
+            <Image
+               width={64}
+               height={50}
+               className="w-64 mb-8 md:mb-0"
+               src="logo.svg"
+               alt="logo react node send"
+               priority
+            />
+         </Link>
+
+         <div>
+            <Link href="/login" className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-5">
+               Iniciar Sesión
+            </Link>
+            <Link href="/createaccount" className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">
+               Crear cuenta
+            </Link>
+         </div>
       </header>
    )
 }
