b5617c10c5cb1c0da4fbe7958a9aaa5283e11c1d
Rencas <rencasdag.12@gmail.com>
2023-09-03 12:46:45 -0500

    auth user + show messages

--- client/context/auth/authReducer.js
+++ client/context/auth/authReducer.js
@@ -1,16 +1,26 @@
 import {
    SUCCESFUL_REGISTRATION,
    ERROR_REGISTRATION,
-   CLEAR_ALERT
+   CLEAR_ALERT,
+   SUCCESFUL_LOGIN,
+   ERROR_LOGIN
 } from '@/types'
 
 export default (state, action) => {
    switch (action.type) {
       case SUCCESFUL_REGISTRATION:
       case ERROR_REGISTRATION:
+      case ERROR_LOGIN:
          return {
             ...state,
             message: action.payload
+         }
+      case SUCCESFUL_LOGIN:
+         localStorage.setItem('token', action.payload);
+         return {
+            ...state,
+            token: action.payload,
+            authenticated: true
          }
       case CLEAR_ALERT:
          return {
